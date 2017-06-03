219748fe59776d31b3035576bb35e54f442382b9
Hatim Kanchwala <hatim@hatimak.me>
2017-06-04 00:38:42 +0530

    Initial L2 Memory with functional logic

--- src/edsac/memory/memory.v
+++ src/edsac/memory/memory.v
@@ -1,17 +1,34 @@
+/* The EDSAC had 32 memory tanks, each designed to hold 
+ * 16 long words (each 36-bit wide). It took 1.152 ms for 
+ * one complete circulation. Thus 512 long words could 
+ * be stored in all. Later in its lifetime, the capacity 
+ * was doubled to 1024 long words.
+ * 
+ * The 32 tanks are distributed across 4 racks.
+ */
+
 module memory
-  (output reg monitor,
-   output reg rack_loc_mob_t,
+  #(parameter STORE_LEN = 16,
+    parameter WORD_WIDTH = 36
+   )
+   (output wire [STORE_LEN*WORD_WIDTH-1:0] monitor,
+    output wire rack_loc_mob_t,
 
-   input wire rack_clk,
-   input wire rack_mib,
-   input wire rack_loc_t_clr,
-   input wire rack_loc_t_in,
-   input wire rack_loc_t_out);
+    input wire  rack_clk,
+    input wire  rack_mib,
+    input wire  rack_loc_t_in,
+    input wire  rack_loc_t_clr,
+    input wire  rack_loc_t_out
+   );
 
-   // Atomic dleay_line module to be instantiated here 
-   // with 1.152ms delay for circulation of 16 full 
-   // words or 32 short words (instructions).
+   delay_line #(STORE_LEN, WORD_WIDTH) dl
+     (.monitor      (monitor),
+      .clk          (rack_clk),
+      .data_in      (rack_mib),
+      .data_in_gate (rack_loc_t_in),
+      .data_clr     (rack_loc_t_clr)
+     );
 
-   // Body
+   assign rack_loc_mob_t = rack_loc_t_out ? monitor[0] : 1'bz;
 
-endmodule // memory
+endmodule
