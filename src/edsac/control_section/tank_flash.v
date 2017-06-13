93f90da96c3032170c1a618fa41bedcfe5d09b23
Hatim Kanchwala <hatim@hatimak.me>
2017-06-13 22:31:15 +0530

    Update native L2 Tank Number Flashing Unit with resetting arrangement explanation

--- src/edsac/control_section/tank_flash.v
+++ src/edsac/control_section/tank_flash.v
@@ -1,8 +1,14 @@
 /* Flipflops in Tank Number Flashing Unit are set by gating either 
  * SCT (Stage 1 of main control) or Order (from Order Tank, Stage 2 
- * of main control).
+ * of main control). The Tank Number Flashing Unit is responsivble 
+ * for selecting and providing a route to a particular tank (while CU 
+ * is responsible to select the position within the 16 minor cycles 
+ * the tank).
  * 
- * TODO: Resetting arrangement NOT clear.
+ * Clearly, the Tank Number Flashing Unit needs to be engaged 
+ * in both Stage I (instruction fetch) and Stage II (instruction 
+ * execution) of main control. EPSEP resets flipflops at end of Stage II, 
+ * while inverted g12 resets flipflops at end of Stage I.
  */
 
 module tank_flash
@@ -22,76 +28,64 @@ module tank_flash
    input wire  d27,
    input wire  d28,
    input wire  d29,
-   input wire  epsep,
+   input wire  epsep, // Indicates end of Stage II
    input wire  g12, // Stage 1 of main control.
    input wire  order_sct
   );
 
-   wire reset;
-   wire set11;
-   wire set10;
-   wire set9;
-   wire set8;
-   wire set7;
-   wire set2;
-   wire set1;
+   wire       reset;
+   wire [6:0] tank_addr;
 
+   // Tank address flipflops.
    flipflop ff11
      (.out     (f11_pos),
       .out_bar (), // Unconnected.
-      .set     (set11),
+      .set     (tank_addr[4]),
       .reset   (reset)
      );
-
    flipflop ff10
      (.out     (f10_pos),
       .out_bar (), // Unconnected.
-      .set     (set10),
+      .set     (tank_addr[3]),
       .reset   (reset)
      );
-
    flipflop ff9
      (.out     (f9_pos),
       .out_bar (), // Unconnected.
-      .set     (set9),
+      .set     (tank_addr[2]),
       .reset   (reset)
      );
-
    flipflop ff8
      (.out     (f8_pos),
       .out_bar (), // Unconnected.
-      .set     (set8),
+      .set     (tank_addr[1]),
       .reset   (reset)
      );
-
    flipflop ff7
      (.out     (f7_pos),
       .out_bar (), // Unconnected.
-      .set     (set7),
+      .set     (tank_addr[0]),
       .reset   (reset)
      );
 
+   // Odd or even short word.
    flipflop ff2
      (.out     (f2_pos),
       .out_bar (), // Unconnected.
-      .set     (set2),
+      .set     (tank_addr[5]),
       .reset   (reset)
      );
 
+   // Instruction length.
    flipflop ff1
      (.out     (f1_pos),
       .out_bar (f1_neg),
-      .set     (set1),
+      .set     (tank_addr[6]),
       .reset   (reset)
      );
 
-   assign set11 = order_sct & d29;
-   assign set10 = order_sct & d28;
-   assign set9  = order_sct & d27;
-   assign set8  = order_sct & d26;
-   assign set7  = order_sct & d25;
-   assign set2  = order_sct & d20;
-   assign set1  = order_sct & d19;
+   assign tank_addr[4:0] = {5{order_sct}} & {d29, d28, d27, d26, d25};
+   assign tank_addr[6:5] = {2{order_sct}} & {d19, d20};
 
    assign reset = epsep | ~g12;
 
