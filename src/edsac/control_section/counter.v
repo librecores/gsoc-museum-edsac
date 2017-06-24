134ce3c3696c50a2789a36ee0b6a1989aed0f743
Hatim Kanchwala <hatim@hatimak.me>
2017-06-24 23:30:01 +0530

    Update Counter description

--- src/edsac/control_section/counter.v
+++ src/edsac/control_section/counter.v
@@ -5,7 +5,7 @@
  * 
  * TODO: How is the functioning of Starter Unit and SCT related?
  * 
- * TODO: (very important!) Does the half adder in Counter have a 1 p.i. 
+ * TODO: Does the half adder in Counter have a 1 p.i. 
  *       delay that half adders in the machine are usually subject to? 
  *       To answer this, consider the CU. Coincidence is sought only 
  *       within the window d2 to d7 (excluding d7), which means the inputs 
@@ -14,6 +14,16 @@
  *       increments twice every minor cycle, at d1 and d19. Thus, Conuter output 
  *       must arrive 1 p.i. later. This is a hypothesis and needs to be validated, 
  *       but for now the modules assume this hypothesis to be true.
+ * 
+ * TODO: The original report says that the Counter keeps count of minor cycles, 
+ *       which would imply that it should be incremented once every minor cycle. 
+ *       The logic diagram, however, shows increment twice every minor cycle.
+ *       The d2 pulse causes the increment. It should be noted that coincidene 
+ *       between counter and order/SCT is sought in the CU during d2 to d6 (both 
+ *       inclusive), so it would make sense that d2 become the LSB of Counter.
+ * 
+ * TODO: The logic diagram description says the Counter is incremented at d2 and 
+ *       d20, whereas the diagram itself depicts d1 and d19 to increment the Counter.
  */
 
 module counter
