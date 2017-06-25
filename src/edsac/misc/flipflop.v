b308ce3fed00eea6a834ad16dd6d90960e0e9596
Hatim Kanchwala <hatim@hatimak.me>
2017-06-25 22:32:05 +0530

    Make flipflop synthesisable on myStorm

--- src/edsac/misc/flipflop.v
+++ src/edsac/misc/flipflop.v
@@ -1,22 +1,21 @@
-module flipflop
-   (output wire out,
-    output wire out_bar,
+module flipflop (
+  output wire out,
+  output wire out_bar,
 
-    input wire  set,
-    input wire  reset
-   );
+  input wire  set,
+  input wire  reset
+  );
 
-   reg outi = 1'b0;
+  reg  outi = 1'b0;
 
-   always @(posedge set) begin
+  always @(posedge set or posedge reset) begin
+    if (set)
       outi <= 1'b1;
-   end
-
-   always @(posedge reset) begin
+    else
       outi <= 1'b0;
-   end
+  end
 
-   assign out = outi;
-   assign out_bar = ~out;
+  assign out = outi;
+  assign out_bar = ~out;
 
 endmodule
