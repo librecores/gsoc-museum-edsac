104f92e03b3b96491c895beaa1c48942fb193eb0
Hatim Kanchwala <hatim@hatimak.me>
2017-07-25 07:25:32 +0530

    Stub module for L2 Control Swithces Unit

---
+++ src/edsac/input/control_switches.v
@@ -0,0 +1,22 @@
+/* Module for Control Switches Unit.
+ */
+
+module contol_switches (
+  output wire epsep,
+  output wire ep11,
+  output wire extended_pos,
+  output wire extended_neg,
+  output wire single_ep,
+  output wire start,
+  output wire stop_neg,
+
+  input wire  s2,
+  input wire  c22,
+  input wire  d18,
+  input wire  d35,
+  input wire  ep,
+  input wire  starter_neg,
+  input wire  sep2
+  );
+
+endmodule
