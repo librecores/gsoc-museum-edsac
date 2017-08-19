c4cefa1e575037aa81307628e69a458cb0b8ce0d
Hatim Kanchwala <hatim@hatimak.me>
2017-08-19 19:56:31 +0530

    Add 500KHz clock for EDSAC

--- src/edsac/chip.v
+++ src/edsac/chip.v
@@ -25,4 +25,20 @@
   // PMOD[55] is LED1 on board, PMOD[54] is LED2, and so on.
   assign PMOD[55:0] = {56{1'bz}};
 
+  // EDSAC ran at a clock of 500KHz.
+  reg [7:0] count = 0;
+  reg clk_edsac   = 0;
+
+  always @(posedge clk) begin
+    count[7:0] <= count[7:0] + 1;
+    if (count[7:0] == 8'b1100_1000) begin
+      clk_edsac <= ~clk_edsac;
+      count[7:0] <= 0;
+    end
+  end
+
+  edsac edsac (
+    .clk (clk_edsac)
+    );
+
 endmodule
