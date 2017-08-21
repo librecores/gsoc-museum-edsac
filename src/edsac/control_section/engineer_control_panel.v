788e6767c485b2cc0480f2ecb8392cdd1cb3c325
Hatim Kanchwala <hatim@hatimak.me>
2017-08-21 23:05:52 +0530

    Update Engineers Control Panel to native level

--- src/edsac/control_section/engineer_control_panel.v
+++ src/edsac/control_section/engineer_control_panel.v
@@ -45,4 +45,36 @@ module engineer_control_panel (
   input wire  eng_mode_t_btn
   );
 
+  wire eng_out;
+  wire ff_out;
+
+  assign eng_out = (o1_t_btn & d19) |
+                   (o2_t_btn & d20) |
+                   (o3_t_btn & d21) |
+                   (o4_t_btn & d22) |
+                   (o5_t_btn & d23) |
+                   (o6_t_btn & d24) |
+                   (o7_t_btn & d25) |
+                   (o8_t_btn & d26) |
+                   (o9_t_btn & d27) |
+                   (o10_t_btn & d28) |
+                   (o11_t_btn & d29) |
+                   (o12_t_btn & d30) |
+                   (o13_t_btn & d31) |
+                   (o14_t_btn & d32) |
+                   (o15_t_btn & d33) |
+                   (o16_t_btn & d34) |
+                   (o17_t_btn & d35);
+
+  assign eng_mode_neg = ~eng_mode_t_btn;
+  assign eng_order = eng_out & eng_mode_t_btn;
+  assign order_clr = ff_out & eng_mode_t_btn;
+
+  flipflop ff (
+    .out     (ff_out),
+    .out_bar (), // Unconnected.
+    .set     (d18),
+    .reset   (d0)
+    );
+
 endmodule
